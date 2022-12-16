cca1037a06914c674e851e430f18a0271cdcc1a9
Clair Penido <clairpenido@gmail.com>
2022-12-16 10:12:11 -0300

    atualizações

--- src/middlewares/productValidation.ts
+++ src/middlewares/productValidation.ts
@@ -4,15 +4,15 @@ import { NextFunction, Request, Response } from 'express';
 import CustomError from '../utils/CustomError';
 
 export default (req: Request, res: Response, next: NextFunction) => {
-  const loginSchema = Joi.object({
-    username: Joi.string().required(),
-    password: Joi.string().required(),
+  const productSchema = Joi.object({
+    name: Joi.string().required().min(2),
+    amount: Joi.string().required().min(2),
   });
-  const { username, password } = req.body;
-  const { error } = loginSchema.validate({ username, password });
+  const { name, amount } = req.body;
+  const { error } = productSchema.validate({ name, amount });
 
   if (error) {
-    console.log('error:', error);
+    console.log('erro Joi:', error);
     throw new CustomError(error.details[0].message, 400);
   }
   next();
--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import productValidation from '../middlewares/productValidation';
 
 const router = Router();
 const productController = new ProductController();
 
 router.get('/', productController.getAllProducts);
-router.post('/', productController.createProduct);
+router.post('/', productValidation, productController.createProduct);
 
 export default router;
